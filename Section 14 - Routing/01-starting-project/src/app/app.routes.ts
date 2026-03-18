ea6d54dc351191e9092cc278b4001565b96bc3c3
Thiago Borsatto <thiagoborsattodutra14@gmail.com>
2026-03-18 14:18:25 -0300

    Angular Section 14: How to use router to change the UI elements and pass data with routing wrapper.

---
+++ Section 14 - Routing/01-starting-project/src/app/app.routes.ts
@@ -0,0 +1,47 @@
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
+
+import { routes as userRoutes} from "./users/user.routes";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummyCanMatch : CanMatchFn = (route, segment) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+
+    if (shouldGetAccess < 0.95) {
+        return true;
+    }
+
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
+
+export const routes: Routes = [
+    {
+        path: '', // <your-domain>
+        component: NoTaskComponent,
+        // redirectTo: '/users/u1',
+        // pathMatch: 'full'
+        title: 'No tasks selected',
+    },
+    {
+        path: 'users/:userId', // <your-domain>/users/<uid>
+        component: UserTasksComponent,
+        children: userRoutes,
+        canMatch: [
+            dummyCanMatch
+        ],
+        data: {
+            message: 'Hello!'
+        },
+        resolve: {
+           userName: resolveUserName,
+        },
+        title: resolveTitle,
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    },
+]
---
+++ Section 14 - Routing/01-starting-project/src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,67 @@
+import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet, RouterLink, ResolveFn, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink],
+})
+export class UserTasksComponent {
+  userName = input.required<string>();
+  message = input.required<string>();
+  private activatedRoute = inject(ActivatedRoute);
+  
+  // Other forms to receive ROUTERS data. 
+  // ngOnInit() {
+  //   this.activatedRoute.data.subscribe({
+  //     next: (data) => {
+  //       console.log(data);
+  //     } 
+  //   });
+  // }
+
+  // private usersService = inject(UsersService);
+  // private destroyRef = inject(DestroyRef);
+  // userId = input.required<string>();
+
+  // ngOnInit() {
+  //   console.log('Input Data: ' + this.message());
+  //   console.log(this.activatedRoute);
+  //   const subscription = this.activatedRoute.paramMap.subscribe({
+  //     next: (paramMap) => {
+  //       this.userName = this.usersService.users.find(
+  //         (u) => u.id === paramMap.get('userId')
+  //       )?.name || '';
+  //     },
+  //   })
+
+  //   this.destroyRef.onDestroy(() => {
+  //     subscription.unsubscribe();
+  //   });
+  // }
+  
+  // userName = computed(
+  //   () => this.userService.users.find(u => u.id === this.userId())?.name
+  // );
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot, 
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName = usersService.users.find(
+          (u) => u.id === activatedRoute.paramMap.get('userId'))?.name || '';
+  return userName;
+};
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute,
+  routerState
+) => {
+  return resolveUserName(activatedRoute, routerState) + '\'s Tasks'
+}
