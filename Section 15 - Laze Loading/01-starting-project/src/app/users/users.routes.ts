70bb92ef6fe66e2d29b4e0a5709413d10c3431e7
Thiago Borsatto <thiagoborsattodutra14@gmail.com>
2026-03-19 08:42:53 -0300

    Angular Section 15: Learning about Laze Loading, Section: 16: Deploying a Angular App

---
+++ Section 15 - Laze Loading/01-starting-project/src/app/users/users.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+
+import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service';
+import { resolveUserTasks } from '../tasks/tasks.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        // loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+        component: TasksService,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
+  },
+];
